6b9f8b3aead2f481ac59a4299daf2b684c551e9c
niicku97 <nick_97_seven@yahoo.com>
2020-07-16 00:46:07 +0300

    Updates

---
+++ precache-manifest.3fa56a33f62f79b261f4dbdd80ebaa8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7e970c608476fe3771ffb3590f6fae0",
+    "url": "/shopping_list/index.html"
+  },
+  {
+    "revision": "58e64e8fce6ed8eba9bd",
+    "url": "/shopping_list/static/css/main.4ade98ef.chunk.css"
+  },
+  {
+    "revision": "e8a41dc74b3a3dc732f3",
+    "url": "/shopping_list/static/js/2.a2de673a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/shopping_list/static/js/2.a2de673a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58e64e8fce6ed8eba9bd",
+    "url": "/shopping_list/static/js/main.20e0ce60.chunk.js"
+  },
+  {
+    "revision": "943179c62454e59f5c41",
+    "url": "/shopping_list/static/js/runtime-main.533fe0d1.js"
+  }
+]);
